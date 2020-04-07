2c9ec028f9cd120bbb3c30313dccf1b9e5858f84
Miguel Hermida Cores <miguel.hermida@saldum.com>
2020-04-08 01:40:02 +0200

    Print and update keywords list

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,14 +1,44 @@
-const SWITCH_SELECTOR = ".switch__input";
-const LOGO_SELECTOR = ".header__logo";
+const popupSwitch = document.querySelector(".switch__input");
+const headerLogo = document.querySelector(".header__logo");
+const keywordsInput = document.querySelector(".keywords__field");
+const keywordsSubmit = document.querySelector(".keywords__submit");
+const keywordsBlock = document.querySelector(".keywords__block");
+const keywordsRemove = document.querySelector(".keywords__item--remove");
+const keywordsReset = document.querySelector(".keywords__reset--button");
 
-const popupSwitch = document.querySelector(SWITCH_SELECTOR);
+let keywordsList = [];
 
-// chrome.storage.sync.get("color", function (data) {
-//   changeColor.style.backgroundColor = data.color;
-//   changeColor.setAttribute("value", data.color);
-// });
-
-popupSwitch.onclick = function (element) {
-  const headerLogo = document.querySelector(LOGO_SELECTOR);
+popupSwitch.onclick = function () {
   headerLogo.classList.toggle("disabled");
+};
+
+keywordsSubmit.onclick = function () {
+  if (keywordsInput.value != "") {
+    keywordsList.push(keywordsInput.value);
+    keywordsInput.value = "";
+    updateList();
+  }
+};
+
+// keywordsRemove.onclick = function () {
+//   console.log(keywordsList[this.dataset.index]);
+// };
+
+keywordsReset.onclick = function () {
+  keywordsList = [];
+  keywordsBlock.innerHTML = "";
+};
+
+const updateList = function () {
+  let keywordsContent = "";
+  for (i = 0; i < keywordsList.length; i++) {
+    keywordsContent +=
+      "<span class='keywords__item'>" +
+      keywordsList[i] +
+      "<img data-index='" +
+      i +
+      "' class='keywords__item--remove' src='/src/images/close.svg'/>" +
+      "</span>";
+  }
+  keywordsBlock.innerHTML = keywordsContent;
 };
